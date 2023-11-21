72a927783016ae47a0e3fd3c3c7351258968d8c4
MUHAMMAD OSAMA SALEEM <osaleem666@gmail.com>
2023-11-21 20:05:22 +0500

    first API

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import json
 import requests
 
+CardNumber = 9900000067544760269
+CardNumber = str(CardNumber)
 
-## get card series number and total cards
-baseURL = 'http://gcdev.matas.dk/'
-getCardSeriesAPI =
+baseURL = 'https://gcdev.matas.dk'
+
+# get card series number and total cards
+
+getCardSeriesNumberAPI = baseURL + f'/v1/series/listPagination?query={CardNumber}&start=0&limit=10'
+print(getCardSeriesNumberAPI)
+
+getCardSeriesNumberAPIResponse = requests.get(getCardSeriesNumberAPI, timeout=20)
+print(getCardSeriesNumberAPIResponse)
+
